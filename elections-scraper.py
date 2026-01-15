a9fb429e268d76cd3002c0530369a909e48b728d
ccoop129 <chriscoop129@gmail.com>
2026-01-15 18:45:35 -0500

    Task 2, print years

--- elections-scraper.py
+++ elections-scraper.py
@@ -7,9 +7,20 @@ import requests
 url = "https://electionstats.state.ma.us/elections/search/year_from:2024/year_to:2025"
 
 # get the page
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+response = requests.get(url, headers=headers)
 
 # extract HTML as structured "soup"
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
 
 # get each row of the table (what is the css selector for the whole row?)
+elections = soup.select("tr.election_item")
+
 
 # Loop through the rows and extract the year
+for election in elections:
+    year = election.select_one("td.year").text.strip()
+    print(year)
